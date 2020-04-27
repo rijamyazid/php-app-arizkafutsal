1f31a512277893ee24466657e2df25888421c6da
rijamyazid <rijamyazid@gmail.com>
2020-04-27 19:47:11 +0700

    Pemesanan [BACKEND] fix-2.0

--- Code/Pemesanan/pesan.php
+++ Code/Pemesanan/pesan.php
@@ -19,17 +19,17 @@
             Apakah Anda ingin memesan lapangan pada waktu dibawah ?
             Lapangan    : <?php if($_GET['idLapang']==1) echo "Syntesis"; else echo "Vinyl;" ?><br>
             Hari/Tanggal: <?= reverseDate($_GET['tanggal']) ?><br>
-            Waktu       : <?= convertIdToWaktu($con, $_GET['idJadwal']) ?><br>
+            Waktu       : <?= getWaktuByIdWaktu($con, $_GET['idJadwal']) ?><br>
             Pembayaran  : Rp. 100.000
         </pre>
         <div>
-        <input type="submit" class="ijo" name="setuju" value="Pesan">
+        <form method="POST">
+            <input type="submit" class="ijo" name="setuju" value="Pesan">
+            <input type="submit" class="merah" name="batal" value="batal">
+        </form>
         <?php
-            if(isset($_POST['setuju'])){
-                echo "Setuju";
-            }
+            issetForm($con, $data);
         ?>
-        <input type="submit" class="merah" name="batal" value="batal">
         </div
     </div>
 
@@ -43,10 +43,35 @@
         return $tempDate[2]."-".$tempDate[1]."-".$tempDate[0];
     }
 
-    function convertIdToWaktu($con, $idJadwal){
+    function getWaktuByIdWaktu($con, $idJadwal){
         $query = "SELECT * FROM jadwal WHERE id_jadwal='$idJadwal'";
         $result = mysqli_query($con, $query);
         $data = mysqli_fetch_array($result);
         return $data['waktu'];
+    }
+
+    function issetForm($con, $userData){
+        if(isset($_POST['setuju'])){
+            $query = "INSERT INTO transaksi (tanggal, id_jadwal, username, id_lapang, total_bayar) VALUES 
+                    ('$_GET[tanggal]', '$_GET[idJadwal]', '$_SESSION[username]', '$_GET[idLapang]', '100000')";
+            $result = mysqli_query($con, $query);
+
+            if($userData['saldo'] >= 100000){
+                $saldoReduced = $userData['saldo'] - 100000;
+                $query = "UPDATE user SET saldo='$saldoReduced' WHERE username='$_SESSION[username]'";
+                $result = mysqli_query($con, $query);
+
+                echo '<p>Pemesanan berhasil, anda akan dialihkan ke beranda</p>';
+                echo "<meta http-equiv='refresh' content='2;url=../member/index.php'>";
+            } else {
+                echo '<p>Pemesanan gagal, saldo anda tidak cukup</p>';
+                echo "<meta http-equiv='refresh' content='2;url=../member/index.php?hal=jadwal_sintesis'>";
+            }
+        } else if(isset($_POST['batal'])){
+
+            echo '<p>Pemesanan dibatalkan, anda akan dialihkan ke daftar jadwal</p>';
+            echo "<meta http-equiv='refresh' content='2;url=../member/index.php?hal=jadwal_sintesis'>";
+
+        }
     }
 ?>
